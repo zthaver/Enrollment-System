5df2fadb02b24ca775efc06b4c7bb02ee41012f5
Zain Thaver <47486742+zthaver@users.noreply.github.com>
2021-07-09 16:03:43 -0400

    experimenting with scheduler

--- src/components/Professor/SubmitAvailbility/SubmitAvailibility.js
+++ src/components/Professor/SubmitAvailbility/SubmitAvailibility.js
@@ -1,107 +1,176 @@
-import * as React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import {
-  Scheduler,
-  WeekView,
-  Appointments,
-  DragDropProvider,
-  EditRecurrenceMenu,
-  AllDayPanel,
-} from '@devexpress/dx-react-scheduler-material-ui';
 
-const currentDate = '2018-11-01';
-const recurrenceAppointments = [{
-  title: 'Website Re-Design Plan',
-  startDate: new Date(2018, 5, 25, 9, 15),
-  endDate: new Date(2018, 5, 25, 11, 30),
-  id: 100,
-  rRule: 'FREQ=DAILY;COUNT=3',
-  exDate: '20180628T063500Z,20180626T061500Z',
-}, {
-  title: 'Book Flights to San Fran for Sales Trip',
-  startDate: new Date(2018, 5, 25, 12, 11),
-  endDate: new Date(2018, 5, 25, 13, 0),
-  id: 101,
-  rRule: 'FREQ=DAILY;COUNT=4',
-  exDate: '20180627T091100Z',
-  allDay: true,
-}, {
-  title: 'Install New Router in Dev Room',
-  startDate: new Date(2018, 5, 25, 13, 30),
-  endDate: new Date(2018, 5, 25, 14, 35),
-  id: 102,
-  rRule: 'FREQ=DAILY;COUNT=5',
-}, {
-  title: 'Approve Personal Computer Upgrade Plan',
-  startDate: new Date(2018, 5, 26, 10, 0),
-  endDate: new Date(2018, 5, 26, 11, 0),
-  id: 3,
-  location: 'Room 2',
-}, {
-  title: 'Final Budget Review',
-  startDate: new Date(2018, 5, 27, 11, 45),
-  endDate: new Date(2018, 5, 27, 13, 20),
-  id: 4,
-  location: 'Room 2',
-}, {
-  title: 'New Brochures',
-  startDate: new Date(2018, 5, 26, 14, 40),
-  endDate: new Date(2018, 5, 26, 15, 45),
-  id: 5,
-  location: 'Room 2',
-}, {
-  title: 'Install New Database',
-  startDate: new Date(2018, 5, 28, 9, 45),
-  endDate: new Date(2018, 5, 28, 11, 15),
-  id: 6,
-  location: 'Room 1',
-}, {
-  title: 'Approve New Online Marketing Strategy',
-  startDate: new Date(2018, 5, 29, 11, 45),
-  endDate: new Date(2018, 5, 29, 13, 5),
-  id: 7,
-  location: 'Room 3',
-}, {
-  title: 'Create Icons for Website',
-  startDate: new Date(2018, 5, 29, 10, 0),
-  endDate: new Date(2018, 5, 29, 11, 30),
-  id: 12,
-  location: 'Room 2',
-}];
+import Scheduler, {SchedulerData, ViewTypes, DATE_FORMAT} from 'react-big-scheduler'
+//include `react-big-scheduler/lib/css/style.css` for styles, link it in html or import it here
+import 'react-big-scheduler/lib/css/style.css'
+import moment from 'moment'
+import React, {Component} from 'react'
+import withDragDropContext from '../../../withDndContext'
+import DemoData from './DemoData';
+class SubmitAvailibility extends Component{
+    constructor(props){
+        super(props);
 
 
-function SubmitAvailiblity()
+        //let schedulerData = new SchedulerData(new moment("2017-12-18").format(DATE_FORMAT), ViewTypes.Week);
+        let schedulerData = new SchedulerData('2017-12-18', ViewTypes.Week);
+        schedulerData.localeMoment.locale('en');
+        moment.locale('zh-cn');
+schedulerData.setLocaleMoment(moment);
+//set resources here or later
+let resources = [
+                    {
+                       id: 'r0',
+                       name: 'Resource0',
+                       groupOnly: true
+                    },
+                    {
+                       id: 'r1',
+                       name: 'Resource1'
+                    },
+                    {
+                       id: 'r2',
+                       name: 'Resource2',
+                       parentId: 'r0'
+                    },
+                    {
+                       id: 'r3',
+                       name: 'Resource3',
+                       parentId: 'r4'
+                    },
+                    {
+                       id: 'r4',
+                       name: 'Resource4',
+                       parentId: 'r2'
+                    },
+                ];
+let events = [
+    {
+         id: 1,
+         start: '2017-12-18 09:30:00',
+         end: '2017-12-19 23:30:00',
+         resourceId: 'r1',
+         title: 'I am finished',
+         bgColor: '#D9D9D9'
+     },
+     {
+         id: 2,
+         start: '2017-12-18 12:30:00',
+         end: '2017-12-26 23:30:00',
+         resourceId: 'r2',
+         title: 'I am not resizable',
+         resizable: false
+     },
+     {
+         id: 3,
+         start: '2017-12-19 12:30:00',
+         end: '2017-12-20 23:30:00',
+         resourceId: 'r3',
+         title: 'I am not movable',
+         movable: false
+     },
+     {
+         id: 4,
+         start: '2017-12-19 14:30:00',
+         end: '2017-12-20 23:30:00',
+         resourceId: 'r1',
+         title: 'I am not start-resizable',
+         startResizable: false
+     },
+     {
+         id: 5,
+         start: '2017-12-19 15:30:00',
+         end: '2017-12-20 23:30:00',
+         resourceId: 'r2',
+         title: 'R2 has recurring tasks every week on Tuesday, Friday',
+         rrule: 'FREQ=WEEKLY;DTSTART=20171219T013000Z;BYDAY=TU,FR',
+         bgColor: '#f759ab'
+     }
+ ];
+        schedulerData.setResources(DemoData.resources);
+        schedulerData.setEvents(DemoData.events);
+        this.state = {
+            viewModel: schedulerData
+        }
+    }
+  
+//set locale moment to the schedulerData, if your locale isn't English. By default, Scheduler comes with English(en, United States).
+
+render()
 {
-return (
-    <div>
-        <Paper>
-        <Scheduler
-          data={data}
-          height={660}
-        >
-          <ViewState
-            defaultCurrentDate={currentDate}
-          />
-          <EditingState
-            onCommitChanges={this.onCommitChanges}
-          />
-          <EditRecurrenceMenu />
-          <WeekView
-            startDayHour={9}
-            endDayHour={16}
-          />
-          <Appointments
-            appointmentComponent={appointmentComponent}
-          />
-          <AllDayPanel />
-          <DragDropProvider
-            allowDrag={allowDrag}
-          />
-        </Scheduler>
-      </Paper>
-    </div>
+    const {viewModel} = this.state;
+    return (
+    <Scheduler schedulerData={viewModel}
+        prevClick={this.prevClick}
+        nextClick={this.nextClick}
+        onSelectDate={this.onSelectDate}
+        onViewChange={this.onViewChange}
+        eventItemClick={this.eventClicked}
+        newEvent={this.newEvent}
+/>
 )
 }
+     prevClick = (schedulerData) => {
+        schedulerData.prev();
+        schedulerData.setEvents(DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    }
+    
+     nextClick =  (schedulerData) => {
+        schedulerData.next();
+        schedulerData.setEvents(DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    
+    }
+    
+     onSelectDate = (schedulerData, date) =>  {
+        schedulerData.setDate(date);
+        schedulerData.setEvents(DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    }
+     eventClicked = (schedulerData, event) =>  {
+        alert(`You just clicked an event: {id: ${event.id}, title: ${event.title}}`);
+    };
+    
+     onViewChange = (schedulerData, view) =>  {
+        schedulerData.setViewType(view.viewType, view.showAgenda, view.isEventPerspective);
+        schedulerData.setEvents(DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    }
 
-export default SubmitAvailiblity;
+    newEvent = (schedulerData, slotId, slotName, start, end, type, item) => {
+        if(window.confirm(`Do you want to create a new event? {slotId: ${slotId}, slotName: ${slotName}, start: ${start}, end: ${end}, type: ${type}, item: ${item}}`)){
+
+            let newFreshId = 0;
+            schedulerData.events.forEach((item) => {
+                if(item.id >= newFreshId)
+                    newFreshId = item.id + 1;
+            });
+
+            let newEvent = {
+                id: newFreshId,
+                title: 'New event you just created',
+                start: start,
+                end: end,
+                resourceId: slotId,
+                bgColor: 'purple'
+            }
+            schedulerData.addEvent(newEvent);
+            this.setState({
+                viewModel: schedulerData
+            })
+        }
+    }
+
+
+
+}
+
+export default withDragDropContext(SubmitAvailibility);
