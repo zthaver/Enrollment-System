20a32340e065f316308fe94a8b07c5575bec54b0
Joshua Duke <dukejoshuae@gmail.com>
2021-07-21 18:32:37 -0400

    working on submit availability

--- src/components/Professor/SubmitAvailbility/SubmitAvailibility.js
+++ src/components/Professor/SubmitAvailbility/SubmitAvailibility.js
@@ -12,10 +12,12 @@ import Paper from '@material-ui/core/Paper';
 
 function SubmitAvailiblity()
 {
+
   const user = (firebase.auth().currentUser).uid;
   const uid = user;
   const profUser = firebase.firestore().collection("professors").doc(uid);
 
+  // variables for setting the availability
   const [startTime, setStartTime] = useState("");
   const [endTime, setEndTime] = useState("");
   const [day, setDay] = useState("");
@@ -23,14 +25,70 @@ function SubmitAvailiblity()
   const [timesArray, setTimesArray] = useState([]);
   const [timesObj, setTimesObj] = useState({});
 
-
   const time = day + " Start: " + startTime + " - End: " + endTime;
 
   var listItems = timesArray.map((time) =>  <li >
-                                              <span key={time.day}> {time.day} </span>  
-                                              <span key={time.start}> {time.start} </span>  
-                                              <span key={time.end}> {time.end} </span>  
-                                            </li>);
+    <span key={time.day}> {time.day} </span>  
+    <span key={time.start}> {time.start} </span>  
+    <span key={time.end}> {time.end} </span>  
+  </li>);
+
+  // variables for retrieving thr availability from the db
+
+  const [userStartTime, setUserStartTime ] = useState();
+  const [userEndTime, setUserEndTime ] = useState();
+  const [userDay, setUserDay ] = useState();
+
+  const [availabilityArr, setAvailabilityArr] = useState([]);
+
+//   useEffect(() => {
+//     firestore.collection("programs").get().then((programs) => {
+//         setProgramData(programs.docs.map((program => program.data())));
+//     })
+// }, [])
+
+      // if user id exists , get data from firestore
+      useEffect(() => {
+        profUser.get().then((doc) => {
+            setAvailabilityArr(doc.data().availability);
+
+        })
+    }, [])
+    console.log(availabilityArr)
+    console.log(typeof availabilityArr)
+    //   if (user !== null) {
+    //     console.log(uid);
+  
+    //     profUser.get().then((doc) => {
+    //         if (doc.exists) {
+    //             console.log("Document data:", doc.data().availability);           
+    //             setAvailabilityArr(doc.data().availability)
+               
+    //         } else {
+    //             // doc.data() will be undefined in this case
+    //             console.log("No such document!");
+    //         }
+    //     }).catch((error) => {
+    //         console.log("Error getting document:", error);
+    //     });
+  
+    // }
+
+
+
+  // var userAvailability = availabilityArr.map((a) => 
+  //     <li>
+  //       <span key={a.day}> {a.day} </span>
+  //       <span key={a.start}> {a.start} </span>
+  //       <span key={a.end}> {a.end} </span>
+  //     </li>)
+
+// var listItems = timesArray.map((time) =>  <li >
+// <span key={time.day}> {time.day} </span>  
+// <span key={time.start}> {time.start} </span>  
+// <span key={time.end}> {time.end} </span>  
+// </li>);
+
 
 
   function test(){
@@ -141,140 +199,25 @@ return (
         <div >
             {listItems}
         </div>
+
+        {/* View Availabikity */}
+        {/* <p>This is the start time: {userStartTime}</p>
+        <p>This is the start time: {userEndTime}</p>
+        <p>This is the start time: {userDay}</p> */}
+        <div>
+          {/* {userAvailability} */}
+
+          {/* { Object.entries(availabilityArr).map(([key, available]) =>(
+            <div key={available}>
+              <li>{key}</li>
+            </div>
+            ))
+          } */}
+
+        </div>
     </div>
-)
+
+  )
 }
 
 export default SubmitAvailiblity;
-
-// import * as React from 'react';
-// import JqxScheduler, {  jqx } from 'jqwidgets-scripts/jqwidgets-react-tsx/jqxscheduler';
-// import 'jqwidgets-scripts/jqwidgets/styles/jqx.base.css';
-// class SubmitAvailiblity extends React.PureComponent  {
-    
-//     constructor(props) {
-//         super(props);
-//         this.myScheduler = React.createRef();
-//         const appointments = new Array();
-//         const appointment1 = {
-//             calendar: "Room 1",
-//             description: "George brings projector for presentations.",
-//             end: new Date(2018, 10, 23, 16, 0, 0),
-//             id: "id1",
-//             location: "",
-//             start: new Date(2018, 10, 23, 9, 0, 0),
-//             subject: "Quarterly Project Review Meeting"
-//         };
-//         const appointment2 = {
-//             calendar: "Room 2",
-//             description: "",
-//             end: new Date(2018, 10, 24, 15, 0, 0),
-//             id: "id2",
-//             location: "",
-//             start: new Date(2018, 10, 24, 10, 0, 0),
-//             subject: "IT Group Mtg."
-//         };
-//         const appointment3 = {
-//             calendar: "Room 3",
-//             description: "",
-//             end: new Date(2018, 10, 21, 13, 0, 0),
-//             id: "id3",
-//             location: "",
-//             start: new Date(2018, 10, 21, 11, 0, 0),
-//             subject: "Course Social Media"
-//         };
-//         const appointment4 = {
-//             calendar: "Room 2",
-//             description: "",
-//             end: new Date(2018, 10, 23, 18, 0, 0),
-//             id: "id4",
-//             location: "",
-//             start: new Date(2018, 10, 23, 16, 0, 0),
-//             subject: "New Projects Planning"
-//         };
-//         const appointment5 = {
-//             calendar: "Room 1",
-//             description: "",
-//             end: new Date(2018, 10, 25, 17, 0, 0),
-//             id: "id5",
-//             location: "",
-//             start: new Date(2018, 10, 25, 15, 0, 0),
-//             subject: "Interview with James"
-//         };
-//         const appointment6 = {
-//             calendar: "Room 4",
-//             description: "",
-//             end: new Date(2018, 10, 26, 16, 0, 0),
-//             id: "id6",
-//             location: "",
-//             start: new Date(2018, 10, 26, 14, 0, 0),
-//             subject: "Interview with Nancy"
-//         };
-//         appointments.push(appointment1);
-//         appointments.push(appointment2);
-//         appointments.push(appointment3);
-//         appointments.push(appointment4);
-//         appointments.push(appointment5);
-//         appointments.push(appointment6);
-//         const source = {
-//             dataFields: [
-//                 { name: 'id', type: 'string' },
-//                 { name: 'description', type: 'string' },
-//                 { name: 'location', type: 'string' },
-//                 { name: 'subject', type: 'string' },
-//                 { name: 'calendar', type: 'string' },
-//                 { name: 'start', type: 'date' },
-//                 { name: 'end', type: 'date' }
-//             ],
-//             dataType: "array",
-//             id: 'id',
-//             localData: appointments
-//         };
-//         const dataAdapter = new jqx.dataAdapter(source);
-//         this.state = {
-//             appointmentDataFields: {
-//                 description: "description",
-//                 from: "start",
-//                 id: "id",
-//                 location: "location",
-//                 resourceId: "calendar",
-//                 subject: "subject",
-//                 to: "end"
-//             },
-//             date: new jqx.date(2018, 11, 23),
-//             height: 600,
-//             resources: {
-//                 colorScheme: "scheme05",
-//                 dataField: "calendar",
-//                 source: new jqx.dataAdapter(source)
-//             },
-//             source: dataAdapter,
-//             views: [
-//                 'dayView',
-//                 'weekView',
-//                 'agendaView'
-//             ]
-//         };
-//     }
-//      render() {
-//         return (
-//             <JqxScheduler ref={this.myScheduler}
-//                 height={this.state.height}
-//                 date={this.state.date}
-//                 source={this.state.source}
-//                 showLegend={true}
-//                 dayNameFormat={"abbr"}
-//                 resources={this.state.resources}
-//                 view={"agendaView"}
-//                 views={this.state.views}
-//                 appointmentDataFields={this.state.appointmentDataFields}
-//                 onAppointmentAdd={this.onAppointmentAdd} 
-//             />
-//         );
-//     }
-//      onAppointmentAdd(e) {
-//         alert('do something...');
-//         console.log(e.args.appointment.originalData)
-//       }
-// }
-// export default SubmitAvailiblity;
