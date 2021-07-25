a7b855e24e1e378feceb37c92836a5f3104abe62
Zain Thaver <47486742+zthaver@users.noreply.github.com>
2021-07-25 19:59:02 -0400

    somewhat functioning scheduler

--- src/components/Professor/SubmitAvailbility/SubmitAvailibility.js
+++ src/components/Professor/SubmitAvailbility/SubmitAvailibility.js
@@ -1,12 +1,35 @@
-import * as React from 'react';
+import {react,useEffect,useState,useRef} from 'react';
 import JqxScheduler, {  jqx } from 'jqwidgets-scripts/jqwidgets-react-tsx/jqxscheduler';
 import 'jqwidgets-scripts/jqwidgets/styles/jqx.base.css';
 import { firestore } from '../../../firebase';
-class SubmitAvailiblity extends React.PureComponent  {
-    
-    constructor(props) {
-        super(props);
-        this.myScheduler = React.createRef();
+function SubmitAvailiblity ()  {
+
+    let [dataFetched,setDataFetched] = useState(false);
+    let [appointmentData,setAppointmentData] = useState([]);
+
+    useEffect(() => {
+        let arrayAppointments = [];
+        firestore.collection("appointments").get().then((appointmentsData) => {
+       setAppointmentData(appointmentsData.docs.map((appointment => 
+              {
+            let convertedAppointment = {};
+            appointment.data();
+            convertedAppointment.start = appointment.get("start").toDate();
+            convertedAppointment.end = appointment.get("end").toDate();
+            convertedAppointment.description = appointment.get("description");
+            convertedAppointment.subject = appointment.get("subject");
+            arrayAppointments.push(convertedAppointment);
+               return convertedAppointment;
+               
+
+            }
+            
+            
+       )));
+       setDataFetched(true);
+    }, [])})
+
+        let myScheduler = useRef();
         const appointments = new Array();
         const appointment1 = {
             calendar: "Room 1",
@@ -17,82 +40,6 @@ class SubmitAvailiblity extends React.PureComponent  {
             start: new Date(2018, 10, 23, 9, 0, 0),
             subject: "Quarterly Project Review Meeting"
         };
-        const appointment2 = {
-            calendar: "Room 2",
-            description: "",
-            end: new Date(2018, 10, 24, 15, 0, 0),
-            id: "id2",
-            location: "",
-            start: new Date(2018, 10, 24, 10, 0, 0),
-            subject: "IT Group Mtg."
-        };
-        const appointment3 = {
-            calendar: "Room 3",
-            description: "",
-            end: new Date(2018, 10, 21, 13, 0, 0),
-            id: "id3",
-            location: "",
-            start: new Date(2018, 10, 21, 11, 0, 0),
-            subject: "Course Social Media"
-        };
-        const appointment4 = {
-            calendar: "Room 2",
-            description: "",
-            end: new Date(2018, 10, 23, 18, 0, 0),
-            id: "id4",
-            location: "",
-            start: new Date(2018, 10, 23, 16, 0, 0),
-            subject: "New Projects Planning"
-        };
-        const appointment5 = {
-            calendar: "Room 1",
-            description: "",
-            end: new Date(2018, 10, 25, 17, 0, 0),
-            id: "id5",
-            location: "",
-            start: new Date(2018, 10, 25, 15, 0, 0),
-            subject: "Interview with James"
-        };
-        const appointment6 = {
-            calendar: "Room 4",
-            description: "",
-            end: new Date(2018, 10, 26, 16, 0, 0),
-            id: "id6",
-            location: "",
-            start: new Date(2018, 10, 26, 14, 0, 0),
-            subject: "Interview with Nancy"
-        };
-        this.state = {
-            appointmentDataFields: {
-                description: "description",
-                from: "start",
-                id: "id",
-                location: "location",
-                resourceId: "calendar",
-                subject: "subject",
-                to: "end"
-            },
-            appointmentsScheduled:[],
-            date: new jqx.date(2018, 11, 23),
-            height: 600,
-            resources: {
-                colorScheme: "scheme05",
-                dataField: "calendar",
-                source: new jqx.dataAdapter(source)
-            },
-            source: dataAdapter,
-            views: [
-                'dayView',
-                'weekView',
-                'agendaView'
-            ]
-        };
-        appointments.push(appointment1);
-        appointments.push(appointment2);
-        appointments.push(appointment3);
-        appointments.push(appointment4);
-        appointments.push(appointment5);
-        appointments.push(appointment6);
         const source = {
             dataFields: [
                 { name: 'id', type: 'string' },
@@ -103,61 +50,76 @@ class SubmitAvailiblity extends React.PureComponent  {
                 { name: 'start', type: 'date' },
                 { name: 'end', type: 'date' }
             ],
-            dataType: "array",
+            dataType: 'array',
             id: 'id',
-            localData: appointments
+            localData: appointmentData
         };
         const dataAdapter = new jqx.dataAdapter(source);
-        
-    }
-    componentDidMount()
-    {
-        let arrayAppointments = [];
-        firestore.collection("appointments").get().then((appointmentsData) => {
-       (appointmentsData.docs.map((appointment => 
-              {
-            let convertedAppointment = {};
-            appointment.data();
-            convertedAppointment.start = appointment.get("start").toDate();
-            convertedAppointment.end = appointment.get("end").toDate();
-            convertedAppointment.description = appointment.get("description");
-            convertedAppointment.subject = appointment.get("subject");
-             console.log(convertedAppointment)
-            console.log(appointment.data())
-            arrayAppointments.push(convertedAppointment);
-               return convertedAppointment;
 
-            }
-            
-            
-       )));
-     })
-     this.setState({
-         appointmentsScheduled:arrayAppointments
-     })
-     console.log(this.state.appointmentsScheduled)
-    }
-     render() {
+        let state = {
+            dataFetched:false,
+            appointmentDataFields: {
+                description: "description",
+                from: "start",
+                id: "id",
+                location: "location",
+                resourceId: "calendar",
+                subject: "subject",
+                to: "end"
+            },
+            date: new jqx.date(2018, 11, 23),
+            height: 600,
+            source: dataAdapter,
+            resources: {
+                colorScheme: "scheme05",
+                dataField: "calendar",
+                source: new jqx.dataAdapter(source)
+            },
+            views: [
+                'dayView',
+                'weekView',
+                'agendaView'
+            ]
+        };
+
+
+      
+        appointments.push(appointment1);
+
+    
+      
+        
+    
+
+   
+
+ 
+    
+  
         return (
-            <JqxScheduler ref={this.myScheduler}
-                height={this.state.height}
-                date={this.state.date}
-                source={this.state.source}
+            dataFetched?
+            <JqxScheduler ref={myScheduler}
+                height={state.height}
+                date={state.date}
+                source={state.source}
                 showLegend={true}
                 dayNameFormat={"abbr"}
-                resources={this.state.resources}
+                resources={state.resources}
                 view={"agendaView"}
-                views={this.state.views}
-                appointmentDataFields={this.state.appointmentDataFields}
-                onAppointmentAdd={this.onAppointmentAdd} 
-            />
+                views={state.views}
+                appointmentDataFields={state.appointmentDataFields}
+                onAppointmentAdd={onAppointmentAdd} 
+            />:null
         );
-    }
-     onAppointmentAdd(e) {
+    
+     function onAppointmentAdd(e) {
         alert('do something...');
         console.log(e.args.appointment.originalData)
       }
-}
+    }
+    
+
+
 export default SubmitAvailiblity;
 
 
@@ -273,4 +235,3 @@ export default SubmitAvailiblity;
 
 // }
 // export default SubmitAvailibility;
-
